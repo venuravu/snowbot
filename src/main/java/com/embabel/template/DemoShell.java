9bc187429c1bbf7648ff1ba446fc3ef3674098c8
venuravu <venugopalarao.r@gmail.com>
2026-03-03 07:02:59 +0530

    Initial commit

---
+++ src/main/java/com/embabel/template/DemoShell.java
@@ -0,0 +1,28 @@
+package com.embabel.template;
+
+import com.embabel.agent.api.invocation.AgentInvocation;
+import com.embabel.agent.core.AgentPlatform;
+import com.embabel.agent.domain.io.UserInput;
+import com.embabel.template.agent.WriteAndReviewAgent;
+import com.embabel.template.injected.InjectedDemo;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+
+@ShellComponent
+record DemoShell(InjectedDemo injectedDemo, AgentPlatform agentPlatform) {
+
+    @ShellMethod("Demo")
+    String demo() {
+        // Illustrate calling an agent programmatically,
+        // as most often occurs in real applications.
+        var reviewedStory = AgentInvocation
+                .create(agentPlatform, WriteAndReviewAgent.ReviewedStory.class)
+                .invoke(new UserInput("Tell me a story about caterpillars"));
+        return reviewedStory.getContent();
+    }
+
+    @ShellMethod("Invent an animal")
+    String animal() {
+        return injectedDemo.inventAnimal().toString();
+    }
+}
